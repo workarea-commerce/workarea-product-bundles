6051cc2a47189fc2d8859a26235f63e280422dd1
Matt Duffy <mttdffy@gmail.com>
2020-08-31 14:34:22 -0400

    Convert helper methods to view model methods
    No changelog

--- app/helpers/workarea/storefront/product_bundles_helper.rb
+++ app/helpers/workarea/storefront/product_bundles_helper.rb
@@ -10,14 +10,6 @@ module Workarea
         return field if !product.bundle? || product.package?
         "bundled_items__#{field}"
       end
-
-      def show_bundled_product_add_to_cart?(product)
-        product.package?
-      end
-
-      def bundle_fixed_selection?(product)
-        product.kit?
-      end
     end
   end
 end
